3afed732d2bd963fd33c07e130c09e1f28ac5b35
Alina Phang <aph78@uclive.ac.nz>
2020-03-19 15:25:46 +1300

    Debugging photos

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -234,15 +234,27 @@ exports.setPhoto = async function(req, res){
             const file = fs.createWriteStream(photoDirectory + 'user_sample.jpg');
             req.pipe(file);
 
+            file.on('close', () => {
+                res.end();
+            });
+
             await User.putPhoto(id, 'user_sample.jpg');
         } else if (photoType === 'image/png') {
             const file = fs.createWriteStream(photoDirectory + 'user_sample.png');
             req.pipe(file);
 
+            file.on('close', () => {
+                res.end();
+            });
+
             await User.putPhoto(id, 'user_sample.png');
         } else if (photoType === 'image/gif') {
             const file = fs.createWriteStream(photoDirectory + 'user_sample.gif');
             req.pipe(file);
+
+            file.on('close', () => {
+                res.end();
+            });
 
             await User.putPhoto(id, 'user_sample.gif');
         } else {
